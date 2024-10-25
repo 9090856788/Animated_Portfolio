e8e00c53cf034cc5125df90436a970718ba42ffb
Kanhu@143 <kanhucharansahoo595@gmail.com>
2024-10-26 00:23:41 +0530

    create react app & add the boiler plate code.

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+
+createRoot(document.getElementById("root")).render(
+  <>
+    <App />
+  </>
+);
